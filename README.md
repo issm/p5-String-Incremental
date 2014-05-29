27545d79b5b4c269d4cc8b7b7a5525d902813126
issm <issmxx@gmail.com>
2014-05-29 12:06:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+String::Incremental - It's new $module
+
+# SYNOPSIS
+
+    use String::Incremental;
+
+# DESCRIPTION
+
+String::Incremental is ...
+
+# LICENSE
+
+Copyright (C) issm.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+issm <issmxx@gmail.com>
---
+++ lib/String/Incremental.pm
@@ -0,0 +1,39 @@
+package String::Incremental;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+String::Incremental - It's new $module
+
+=head1 SYNOPSIS
+
+    use String::Incremental;
+
+=head1 DESCRIPTION
+
+String::Incremental is ...
+
+=head1 LICENSE
+
+Copyright (C) issm.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+issm E<lt>issmxx@gmail.comE<gt>
+
+=cut
+
